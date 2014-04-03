b1fecca0b4bf09bab858fde223dbba7fdc70ce31
Hiroyoshi Kurohara <hkurohar@insight-tec.co.jp>
2014-04-03 14:40:51 +0700

    modified the commend

--- prepare_dependency.sh
+++ prepare_dependency.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-# If you don't want to describe the dependencies as the form of "@Grab()"
+# 1. If you don't want to describe the dependencies as the form of "@Grab()"
 #  in the groovy source file, please write required dependencies in this
 #   script as 'grape' command invokation.
-#
+# 2. If you need some additional preparation, please script it here.
 
 which grape > /dev/null 2>&1 || (echo "No usable grape command installed";exit 1)
 
 grape install io.netty netty 3.9.0.Final
+
 
